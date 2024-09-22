133b191e4cf365b845c711868facc8d567788ff8
notvenja24 <benjaminrromo@outlook.es>
2024-09-21 23:29:40 -0300

    SCRUM-13 Crear la visualización de los trabajadores

--- frontend/src/Componentes/Table.jsx
+++ frontend/src/Componentes/Table.jsx
@@ -1,36 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const Table = () => (
-  <table className="table caption-top bg-white rounded mt-2">
-    <caption className="text-dark fs-4">Workers</caption>
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td colSpan="2">Larry the Bird</td>
-        <td>@twitter</td>
-      </tr>
-    </tbody>
-  </table>
-);
+const Table = () => {
+  const [workers, setWorkers] = useState([]);
+
+  useEffect(() => {
+    const fetchWorkers = async () => {
+      try {
+        const response = await axios.get("http://localhost:8000/trabajadores/");
+        setWorkers(response.data);
+      } catch (error) {
+        console.error("Error fetching workers:", error);
+      }
+    };
+
+    fetchWorkers();
+  }, []);
+
+  return (
+    <table className="table caption-top bg-white rounded mt-2">
+      <caption className="text-dark fs-4">Workers</caption>
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        {workers.map((worker, index) => (
+          <tr key={worker.id_trabajador}>
+            <th scope="row">{index + 1}</th>
+            <td>{worker.nombre}</td>
+            <td>@{worker.nombre.toLowerCase()}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Table;
